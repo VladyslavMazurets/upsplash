89dc83badeb605e8686ed9d6a87b18b31b89ded9
VladyslavMazurets <vladmazurec@gmail.com>
2023-03-22 19:11:19 +0100

    added header-bg

--- src/pages/Editorial.tsx
+++ src/pages/Editorial.tsx
@@ -8,23 +8,32 @@ import '../styles/header.scss'
 function Editorial() {
 
     const token = useSelector((state: RootType) => state.token)
-    const { data, isSuccess } = useGetApiDataQuery({
-        url: 'photos/random?orientation=landscape&count=1',
-        token
-    })
+    // const { data, isSuccess } = useGetApiDataQuery({
+    //     url: 'photos/random?orientation=landscape&count=1',
+    //     token
+    // })
 
     return (
         <>
-            {isSuccess &&
-                <div className="header">
-                    <img
-                        src={data[0].urls?.regular}
-                        alt="" className="header__img" />
-                    <div className="header__container">
-                        Unsplash
+            {/* {isSuccess && */}
+            <div className="header">
+                <img
+                    src={'https://images.unsplash.com/photo-1417325384643-aac51acc9e5d?q=75&fm=jpg&w=1080&fit=max'}
+                    alt="" className="header__img" />
+                <div className="header__container">
+                    <div className="header__bg" />
+                    <div className="header__content">
+                        <p className="header__title"> 3D Renders </p>
+                        <span className="header__subtitle">
+                            The Unsplash community continues to push the boundaries of creativity through 3D Renders. From abstract worlds to photo-realistic interiors, this category celebrates exciting 3-dimensional images designed in the software of your choice and rendered into JPEG images.
+                        </span>
+                        <button className="header__btn">
+                            Submit to <strong>3D Renders</strong>
+                        </button>
                     </div>
                 </div>
-            }
+            </div>
+            {/* } */}
         </>
     )
 }
