e9124337b81725ae53b436abb09821a05c48c466
VladyslavMazurets <vladmazurec@gmail.com>
2023-03-22 20:56:51 +0100

    completed creating header

--- src/pages/Editorial.tsx
+++ src/pages/Editorial.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 import { useGetApiDataQuery } from '../store/api/unsplashApi'
 import { RootType } from '../store/store'
@@ -8,32 +9,48 @@ import '../styles/header.scss'
 function Editorial() {
 
     const token = useSelector((state: RootType) => state.token)
-    // const { data, isSuccess } = useGetApiDataQuery({
-    //     url: 'photos/random?orientation=landscape&count=1',
-    //     token
-    // })
+    const { data, isSuccess } = useGetApiDataQuery({
+        url: 'photos/random?orientation=landscape&count=1',
+        token
+    })
 
     return (
         <>
-            {/* {isSuccess && */}
-            <div className="header">
-                <img
-                    src={'https://images.unsplash.com/photo-1417325384643-aac51acc9e5d?q=75&fm=jpg&w=1080&fit=max'}
-                    alt="" className="header__img" />
-                <div className="header__container">
-                    <div className="header__bg" />
-                    <div className="header__content">
-                        <p className="header__title"> 3D Renders </p>
-                        <span className="header__subtitle">
-                            The Unsplash community continues to push the boundaries of creativity through 3D Renders. From abstract worlds to photo-realistic interiors, this category celebrates exciting 3-dimensional images designed in the software of your choice and rendered into JPEG images.
-                        </span>
-                        <button className="header__btn">
-                            Submit to <strong>3D Renders</strong>
-                        </button>
+            {isSuccess &&
+                <div className="header">
+                    <img
+                        src={data[0].urls?.regular}
+                        alt="" className="header__img" />
+                    <div className="header__container">
+                        <div className="header__bg" />
+                        <div className="header__content">
+                            <p className="header__title"> 3D Renders </p>
+                            <span className="header__subtitle">
+                                The Unsplash community continues to push the boundaries of creativity through 3D Renders. From abstract worlds to photo-realistic interiors, this category celebrates exciting 3-dimensional images designed in the software of your choice and rendered into JPEG images.
+                            </span>
+                            <button className="header__btn">
+                                Submit to <strong>3D Renders</strong>
+                            </button>
+                        </div>
+
+                        <div className="header__footer">
+                            <div className="footer__photo">
+                                <Link to="" className="footer__link">Photo</Link>
+                                <span> by </span>
+                                <Link to="" className="footer__link">Guns</Link>
+                            </div>
+                            <div className="footer__license">
+                                <span> Read more about the </span>
+                                <a href="https://unsplash.com/license"
+                                    target="_blank" rel="noreferrer"
+                                    className="footer__link">
+                                    Unsplash License
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-            {/* } */}
+            }
         </>
     )
 }
