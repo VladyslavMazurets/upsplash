5368caf77f771b63975fbaa53d4b8aa9578a0b02
VladyslavMazurets <vladmazurec@gmail.com>
2023-03-26 21:10:00 +0200

    public secret key

--- src/components/Authorization .tsx
+++ src/components/Authorization .tsx
@@ -8,7 +8,7 @@ import { tokenSliceAction } from '../store/reducers/tokenSlice';
 import { RootDispatch, RootType } from '../store/store';
 
 const CLIENT_ID ='UQZCNsUKPoOkiCEIV2kuZm42ttdwyrVJnQkwPcmLdxM';
-const CLIENT_SEACRET = process.env.REACT_APP_SECRET_KEY;
+const CLIENT_SEACRET = 'EwysqYK-nHfIbi-MEoOvJRheLXb0zh9eQpH72zPaudE';
 const AUTORIZE_URL = 'https://unsplash.com/oauth/authorize';
 const REDIRECT_URL = 'https://upsplash.vercel.app';
 
