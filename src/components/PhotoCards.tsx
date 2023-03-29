4c9c5c3121c2f9d43bee0cc195a24c292a5743c7
VladyslavMazurets <vladmazurec@gmail.com>
2023-03-29 17:15:29 +0200

    set redirect url as localhost

--- src/components/PhotoCards.tsx
+++ src/components/PhotoCards.tsx
@@ -1,45 +1,45 @@
-import React, {
-  MutableRefObject,
-  SyntheticEvent,
-  useRef,
-  useEffect,
-  useState,
-} from 'react';
+import React, { MutableRefObject, useRef, useEffect, useState } from 'react';
 import Mesonry from 'react-masonry-css';
 import { useSelector } from 'react-redux';
+import { PropagateLoader } from 'react-spinners';
 
 import '../styles/PhotoCards.scss';
 import { ITopics } from '../models/models';
 import { useGetApiDataQuery } from '../store/api/unsplashApi';
 import { RootType } from '../store/store';
 
-interface IPhotos {
-  photos: ITopics[];
-}
-
 function PhotoCards() {
   const scrollRef = useRef() as MutableRefObject<HTMLDivElement>;
   const [scrollBarPosition, setScrollBarPosition] = useState(0);
+  const [page, setPage] = useState(1);
   // eslint-disable-next-line prettier/prettier
 
   const token = useSelector((state: RootType) => state.token);
-  const { data: photos, isSuccess: photosSuccess } = useGetApiDataQuery({
-    url: 'photos?page=1&per_page=30&order_by=popular',
+  const {
+    data: photos,
+    isSuccess,
+    isLoading,
+  } = useGetApiDataQuery({
+    url: `photos?page=${page}&per_page=30&order_by=popular`,
     token,
   });
 
   useEffect(() => {
     const handleScroll = () => {
-      scrollBarPosition < window.scrollY &&
+      if (window.scrollY >= 4000 && scrollBarPosition + 800 < window.scrollY) {
         setScrollBarPosition(window.scrollY);
+      }
     };
-    console.log(scrollBarPosition);
+    console.log('Hello');
+
     window.addEventListener('scroll', handleScroll);
 
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, [scrollBarPosition]);
+
+  isLoading && <PropagateLoader color="#000000" />;
 
   return (
     <>
@@ -52,13 +52,14 @@ function PhotoCards() {
           {photos?.map((photo: ITopics) => (
             <div key={photo.id} className="photo">
               <img
-                src={photo.urls!.regular}
-                alt={`Photo by ${photo.user!.name}`}
+                src={photo.urls?.regular}
+                alt={`Photo by ${photo.user?.name}`}
               />
             </div>
           ))}
         </Mesonry>
       </div>
+      {console.log('Scroll Position - ', scrollBarPosition)}
     </>
   );
 }
